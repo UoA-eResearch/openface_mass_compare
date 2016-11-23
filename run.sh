e31f66098b683e588be751ebcf7943cae1cb790d
Nick Young <nick.young@auckland.ac.nz>
2016-11-23 13:05:48 +1300

    make generate_pickle.py unbuffered too

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 
 cd /root
 
+export PYTHONUNBUFFERED=1
+
 if [ ! -f "/root/data/data.pickle" ];then
   echo generating pickle
   python generate_pickle.py
 fi
 
-PYTHONUNBUFFERED=1 gunicorn web_server:app --workers 8 --bind=0.0.0.0:8000 --access-logfile - --reload
+gunicorn web_server:app --workers 8 --bind=0.0.0.0:8000 --access-logfile - --reload
